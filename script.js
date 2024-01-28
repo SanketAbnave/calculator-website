9908ca6a0f973bb08afe4cb3ae82c4e788892d0d
SanketAbnave <155060511+SanketAbnave@users.noreply.github.com>
2024-01-28 21:11:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML === '='){
+            string = eval(string);
+            input.value = string;
+        }
+     
+        else if(e.target.innerHTML === 'AC'){
+            string="";
+            input.value = string;
+
+        }
+        
+        else if(e.target.innerHTML === 'DEL'){
+            string = string.slice(0,-1);
+            input.value = string;
+        }
+        else if (e.target.innerHTML === '%') {
+            string = calculatePercentage(string);
+            input.value = string;
+        }   
+       
+        else{string += e.target.innerHTML;
+        input.value = string;
+        }
+    })
+})
+function evaluateExpression(expression) {
+    try {
+        return eval(expression);
+    } catch (error) {
+        return 'Error';
+    }
+}
+
+function calculatePercentage(expression) {
+    try {
+        return eval(expression) / 100;
+    } catch (error) {
+        return 'Error';
+    }
+}
